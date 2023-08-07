2e8c4b98f7d67018785eda0c8e50b18e3263486f
m.zolfaghari <muhamad.zolfaghari@gmail.com>
2023-08-07 14:42:58 +0330

    Project was updated.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ThemeWrapper from "./Components/RTLprovider/ThemeWrapper";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { RouterProvider } from "react-router-dom";
+import { Router } from "./Router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeWrapper>
+        <Provider store={store}>
+          <RouterProvider router={Router} />
+        </Provider>
+      </ThemeWrapper>
+    </>
   );
 }
 
